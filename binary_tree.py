ebf6148a51f80a44e97323176241b57d814c815e
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-03-29 17:34:10 -0400

    Switched ndlqr_Buildsubtree to BinaryNode _init_ and also ndlqr_BuildTree to _init_

--- binary_tree.py
+++ binary_tree.py
@@ -2,6 +2,7 @@ class BinaryNode (object):
     """
     @brief One of the nodes in the binary tree for the rsLQR solver
     """
+    """
     #How do we initialize binaryNode? - Yana
     def _init_(self):
         self.idx # < knot point index
@@ -12,37 +13,36 @@ class BinaryNode (object):
         self.parent # < parent node
         self.left_child # < left child
         self.right_child # < right child
-
-def BuildSubTree(start,len): #because it is static needs to be in node class or tree class? -Yana
+    """
+    #Not sure how do we initialize self.idx and self.parent?
+    def _init_(self,start,len): 
         """
         actualy builds the tree,need to implement the function (Srishti - "implemented this now, but not sure if it's the correct translation from C")
         """
         mid = (len + 1) // 2
         new_len = mid - 1
         if len > 1:
-            root = start + new_len #binaryNode
-            left_root = BuildSubTree(start, new_len)
-            right_root = BuildSubTree(start + mid, new_len)
-            root.left_child = left_root
-            root.right_child = right_root
-            left_root.parent = root
-            right_root.parent = root
-            root.left_inds.start = left_root.left_inds.start
-            root.left_inds.stop = left_root.right_inds.stop
-            root.right_inds.start = right_root.left_inds.start
-            root.right_inds.stop = right_root.right_inds.stop
-            root.level = left_root.level + 1
-            return root
+            self = start + new_len #binaryNode
+            left_root = BinaryNode(start, new_len)
+            right_root = BinaryNode(start + mid, new_len)
+            self.left_child = left_root
+            self.right_child = right_root
+            left_root.parent = self
+            right_root.parent = self
+            self.left_inds.start = left_root.left_inds.start
+            self.left_inds.stop = left_root.right_inds.stop
+            self.right_inds.start = right_root.left_inds.start
+            self.right_inds.stop = right_root.right_inds.stop
+            self.level = left_root.level + 1
         else:
-            k = start.idx
-            start.left_inds.start = k
-            start.left_inds.stop = k
-            start.right_inds.start = k + 1
-            start.right_inds.stop = k + 1
-            start.level = 0
-            start.left_child = None
-            start.right_child = None
-            return start
+            k = self.idx
+            self.left_inds.start = k
+            self.left_inds.stop = k
+            self.right_inds.start = k + 1
+            self.right_inds.stop = k + 1
+            self.level = 0
+            self.left_child = None
+            self.right_child = None
 
 class OrderedBinarytree(object):
     """
@@ -56,15 +56,18 @@ class OrderedBinarytree(object):
     A new tree can be constructed solely given the length of the time horizon, which must
     be a power of two. Use ndlqr_BuildTree(N) to build a new tree, 
     """
+    """
     def _init_(self,nhorizon): # (Srishti - "why does it have nhorizon as input"; 
     #Yana - "Because we need the length of the length of the time horizon to buils the tree")
         self.root # (C type: BinaryNode*) < root of the tree. Corresponds to the "middle" knot point.
         self.node_list = [] # (C type: BinaryNode*) < a list of all the nodes, ordered by their knot point index
         self.num_elements # (C type: int) < length of the OrderedBinaryTree::node_list
         self.depth # (C type: int) < total depth of the tree
+    """
 
     #Can we call it _init_ instead? - Yana
-    def ndlqr_BuildTree(self, nhorizon): # (Srishti - "Is N (of C code) = nhorizon (of Python code)? - I think yes, look at .c file")
+    #originally ndlqr_BuildTree
+    def _init_(self, nhorizon): # (Srishti - "Is N (of C code) = nhorizon (of Python code)? - I think yes, look at .c file")
         """
         @brief Construct a new binary tree for a horizon of length @p N
         
@@ -73,7 +76,7 @@ class OrderedBinarytree(object):
         @return A new binary tree
         """
         assert(isPowerOfTwo(nhorizon))
-
+        self.node_list = []
         for i in range (nhorizon):
             self.node_list[i].idx = i # (Srishti - "don't we have to make node_list of length `nhorizon` first?")
                                     # (Yana - "In python we don't have to specify the length of the list")
@@ -81,7 +84,7 @@ class OrderedBinarytree(object):
         self.depth = math.log2(nhorizon)
         
         # Build the tree
-        self.root = BuildSubTree(node_list, nhorizon - 1)
+        self.root = BinaryNode(node_list, nhorizon - 1)
 
     #don't need ndlqr_FreeeTree (Srishti - "why not?", Yana - "Because we dont need to free malloc?")
       
