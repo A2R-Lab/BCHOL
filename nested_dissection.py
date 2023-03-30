c8dab62ea863ec075407e52a971cfb3b0f641115
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-03-29 20:37:30 -0400

    *adding to nested_dissection

--- nested_dissection.py
+++ nested_dissection.py
@@ -24,7 +24,71 @@ def ndlqr_SolveLeaf(solver, index):
     F.lambda = (F.lambda*-1)
     F.state = (np.zeros(())) #Do we know the dimensions of F matrix
     F.input = np.matrix.copy(C.input)
-    Rchol = 
+    Rchol = ndlqr_GetRFactorizon(solver.cholinfo, 0)
+   
+    Rchol = lianlg.cholesky(R)
+    
+    MatrixCholeskySolveWithInfo(R, &F->input, Rchol);  // Fu = R \ Cu
+    MatrixCholeskySolveWithInfo(R, &z->input, Rchol);  // zu = R \ zu
+
+    Matrix zy_temp = {nstates, 1,
+                      C->lambda.data};  // grab an unused portion of the matrix data
+    MatrixCopy(&zy_temp, &z->lambda);
+    z.state = z.lambda #MatrixCopy(&z->lambda, &z->state);
+   zy.temp =Q*z.lambda -z*x# MatrixMultiply(Q, &zy_temp, &z->lambda, 0, 0, -1.0,
+                 #  -1.0);  // zy = - Q * zy - zx
+
+   zy_te MatrixCopy(&z->state, &zy_temp);
+    MatrixScaleByConst(&z->state, -1.0);  // zx = -zy
+    ndlqr_GetQFactorizon(solver->cholfacts, 0, &Qchol);
+    MatrixCholeskyFactorizeWithInfo(Q, Qchol);
+
+  } else {
+    int level = 0;
+
+    Q = &solver->diagonals[2 * k];
+    ndlqr_GetQFactorizon(solver->cholfacts, k, &Qchol);
+    MatrixCholeskyFactorizeWithInfo(Q, Qchol);
+
+    ndlqr_GetNdFactor(solver->soln, k, 0, &z);
+
+    // All the terms that don't apply at the last time step
+    if (k < nhorizon - 1) {
+      level = ndlqr_GetIndexLevel(&solver->tree, k);
+      ndlqr_GetNdFactor(solver->data, k, level, &C);
+      ndlqr_GetNdFactor(solver->fact, k, level, &F);
+
+      R = &solver->diagonals[2 * k + 1];
+      ndlqr_GetRFactorizon(solver->cholfacts, k, &Rchol);
+      MatrixCholeskyFactorizeWithInfo(R, Rchol);
+
+      MatrixCholeskySolveWithInfo(R, &z->input,
+                                  Rchol);  // solve zu = R \ zu  (R \ -r)
+      MatrixCopy(&F->state, &C->state);
+      MatrixCholeskySolveWithInfo(Q, &F->state,
+                                  Qchol);  // solve Fx = Q \ Cx  (Q \ A')
+      MatrixCopy(&F->input, &C->input);
+      MatrixCholeskySolveWithInfo(R, &F->input,
+                                  Rchol);  // solve Fu = Q \ Cu  (R \ B')
+    }
+    // Only term at the last time step
+    MatrixCholeskySolveWithInfo(Q, &z->state,
+                                Qchol);  // solve zx = Q \ zx  (Q \ -q)
+
+    // Solve for the terms from the dynamics of the previous time step
+    // NOTE: This is -I on the state for explicit integration
+    //       For implicit integrators we'd use the A2, B2 partials wrt the next
+    //       state and control
+    int prev_level = ndlqr_GetIndexLevel(&solver->tree, k - 1);
+    ndlqr_GetNdFactor(solver->data, k, prev_level, &C);
+    ndlqr_GetNdFactor(solver->fact, k, prev_level, &F);
+    MatrixCopy(&F->state, &C->state);  // the -I matrix
+    MatrixCholeskySolveWithInfo(Q, &F->state,
+                                Qchol);  // solve Q \ -I from previous time step
+    MatrixSetConst(&F->input, 0.0);      // Initialize the B2 matrix to zeros
+  }
+  return 0;
+}
 
 
     return
