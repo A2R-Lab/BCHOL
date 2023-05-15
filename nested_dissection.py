fa0176e554a1aa0f509e4feeefa7c6386a2b8680
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-05-15 12:59:38 -0400

    Update nested_dissection.py

--- nested_dissection.py
+++ nested_dissection.py
@@ -16,9 +16,9 @@ def ndlqr_SolveLeaf(solver, index):
   nhorizon = solver.nhorizon
   k = index 
   if (index == 0):
-    C = ndlqr_GetNdFactor(solver.data,k, 0)
-    F = ndlqr_GetNdFactor(solver.fact,k,0)
-    z = ndlqr_GetNdFactor(solver.soln,k,0)
+    C = solver.data.ndlqr_GetNdFactor(k, 0)
+    F = solver.fact.ndlqr_GetNdFactor(k,0)
+    z = solver.soln.ndlqr_GetNdFactor(k,0)
     Q = solver.diagonals[2*k]
     R = solver.diagonals[2*k+1]
 
@@ -27,45 +27,47 @@ def ndlqr_SolveLeaf(solver, index):
     # [   -I   ] [Fy]   [Cy]   [ 0 ]    [-A'    ]
     # [-I  Q   ] [Fx] = [Cx] = [ A'] => [ 0     ]
     # [      R ] [Fu]   [Cu]   [ B']    [ R \ B']
-    F.lambda = np.matrix.copy(C.state) 
-    F.lambda = (F.lambda*-1)
-    MatrixSetConst(F.state, 0.0)
+    F.lambdas = np.matrix.copy(C.state) 
+    F.lambdas = (F.lambdas*-1)
+    r = len(F.state)
+    c = len(F.state[0])
+    F.state = [[0.0 for j in range(c)] for i in range(r)]
     F.input = np.matrix.copy(C.input)
-    Rchol = solver.cholinfo.ndlqr_GetRFactorizon(0)
-    Rchol = np.linalg.cholesky(R) 
+    Rchol = ndlqr_GetRFactorizon(solver.cholfacts, 0)
+    Rchol = returnCholesky(R)
     MatrixCholeskySolveWithInfo(R,F.input,Rchol) #Fu = R\Cu
     MatrixCholeskySolveWithInfo(R, z.input, Rchol)
    
     #Rchol = lianlg.cholesky(R) What is this doing here?
-    zy_temp = np.matrix.copy(C.lambda) # grab an unused portion of the matrix data
-    zy_temp=np.matrix.copy(z.lambda) #why do we immediately change the matrix?
-    z.lambda = np.matrix.copy(z.state) #MatrixCopy(&z->lambda, &z->state);
-    z.lambda = (-1)*(Q@zy_temp)+z.lambda # MatrixMultiply(Q, &zy_temp, &z->lambda, 0, 0, -1.0,  -1.0);  // zy = - Q * zy - zx
+    zy_temp = np.matrix.copy(C.lambdas) # grab an unused portion of the matrix data
+    zy_temp=np.matrix.copy(z.lambdas) #why do we immediately change the matrix?
+    z.lambdas = np.matrix.copy(z.state) #MatrixCopy(&z->lambda, &z->state);
+    z.lambdas = (-1)*(Q@zy_temp)+z.lambdas # MatrixMultiply(Q, &zy_temp, &z->lambda, 0, 0, -1.0,  -1.0);  // zy = - Q * zy - zx
     z.state =np.matrix.copy(zy_temp)
     z.state = z.state*(-1)
     Qchol = solver.cholfacts
-    Qchol = np.linalg.cholesky(Q) 
+    Qchol = returnCholesky(Q) 
 
   else: #line 60 in srs
     level = 0
 
     Q = solver.diagonals[2 * k]
     Qchol =ndlqr_GetQFactorizon(solver.cholfacts,k) #check how we implemented GetQFactorization
-    Qchol = np.linalg.cholesky(Q)
+    Qchol = returnCholesky(Q)
 
-    z = ndlqr_GetNdFactor(solver.soln,k,0)
+    z = solver.soln.ndlqr_GetNdFactor(k,0)
 
     #All the terms that don't apply at the last time step
     if (k < nhorizon - 1):
-      level = ndlqr_GetIndexLevel(solver.tree, k)
-      ndlqr_GetNdFactor(solver->data, k, level, &C);
-      ndlqr_GetNdFactor(solver->fact, k, level, &F);
+      level = solver.tree.ndlqr_GetIndexLevel(k)
+      C = solver.data.ndlqr_GetNdFactor(k, level)
+      F = solver.fact.ndlqr_GetNdFactor(k, level)
 
-      R = &solver->diagonals[2 * k + 1];
+      R = solver.diagonals[2 * k + 1];
       C = ndlqr_GetRFactorizon(solver.cholfacts, k)
-      Rchol = np.linalg.cholesky(R) 
+      Rchol = returnCholesky(R) 
 
-      MatrixCholeskySolveWithInfo(R, z.input Rchol) # solve zu = R \ zu  (R \ -r)
+      MatrixCholeskySolveWithInfo(R, z.input, Rchol) # solve zu = R \ zu  (R \ -r)
       F.state = np.matrix.copy(C.state)
       MatrixCholeskySolveWithInfo(Q, F.state,Qchol) # solve Fx = Q \ Cx  (Q \ A')
       F.input =np.matrix.copy(C.input)
@@ -78,9 +80,9 @@ def ndlqr_SolveLeaf(solver, index):
     // NOTE: This is -I on the state for explicit integration
     //       For implicit integrators we'd use the A2, B2 partials wrt the next
     //       state and control"""
-    prev_level = ndlqr_GetIndexLevel(solver.tree, k - 1)
-    C = ndlqr_GetNdFactor(solver.data, k, prev_level)
-    F = ndlqr_GetNdFactor(solver.fact, k, prev_level)
+    prev_level = solver.tree.ndlqr_GetIndexLevel(k - 1)
+    C = solver.data.ndlqr_GetNdFactor(k, prev_level)
+    F = solver.fact.ndlqr_GetNdFactor(k, prev_level)
     F.state = np.matrix.copy(C.state)#MatrixCopy(&F->state, &C->state);  // the -I matrix
     MatrixCholeskySolveWithInfo(Q, F.state,Qchol)  # solve Q \ -I from previous time step
     MatrixSetConst(F.input, 0.0) #   // Initialize the B2 matrix to zeros
@@ -112,10 +114,10 @@ Calculates the following:
   @return 0 if successful
 """
 def ndlqr_FactorInnerProduct(data,fact,index,data_level,fact_level):
-  C1=ndlqr_GetNdFactor(data,index, data_level)
-  F1 = ndlqr_GetNdFactor(fact,index,fact_level)
-  C2 = ndlqr_GetNdFactor(data,index+1,data_level)
-  F2 = ndlqr_GetNdFactor(fact,index+1,fact_level)
+  C1 = data.ndlqr_GetNdFactor(index, data_level)
+  F1 = fact.ndlqr_GetNdFactor(index,fact_level)
+  C2 = data.ndlqr_GetNdFactor(index+1,data_level)
+  F2 = fact.ndlqr_GetNdFactor(index+1,fact_level)
   S = F2.ndlqr_GetLambdaFactor()
   S = C1.state@F1.state
   S = C1.input@F1.state + S
@@ -129,35 +131,36 @@ def ndlqr_SolveCholeskyFactor(fact,cholinfo,index,level,upper_level):
     return -1
   if (upper_level<= level):
     sys.stdout.write("ERROR: `upper_level` must be greater than `level`.")
-  F = ndlqr_GetNdFactor(fact,index+1,level)
-  Sbar = F.lambda
-  G = ndlqr_GetNdFactor(fact,index+1,upper_level)
-  f = G.lambda
+  F = fact.ndlqr_GetNdFactor(index+1,level)
+  Sbar = F.lambdas
+  G = fact.ndlqr_GetNdFactor(index+1,upper_level)
+  f = G.lambdas
   MatrixCholeskySolveWithInfo(Sbar,f, cholinfo)
   #return?
 
 def ndlqr_UpdateShurFactor(fact,soln,index,i,level,upper_level,calc_lambda):
   if(fact==None or soln == None): #if (!fact || !soln) is it correct?
-  f_factor = ndlqr_GetNdFactor(soln,index+1,upper_level)
-  g = ndlqr_GetNdFactor(soln, i , upper_level)
-  F = ndlqr_GetNdFactor(fact, i , level)
-  f = f_factor.lambda
+    return -1
+  f_factor = soln.ndlqr_GetNdFactor(index+1,upper_level)
+  g = soln.ndlqr_GetNdFactor(i , upper_level)
+  F = fact.ndlqr_GetNdFactor(i , level)
+  f = f_factor.lambdas
   if(calc_lambda):
-    g.state = (-1)*(F.state@f )+ g.lambda
-  g.state = (-1)*(F.state@f )+ g.lambda
-  g.input = (-1)*(F.state@f )+ g.lambda
+    g.state = (-1)*(F.state@f )+ g.lambdas
+  g.state = (-1)*(F.state@f )+ g.lambdas
+  g.input = (-1)*(F.state@f )+ g.lambdas
   #return 0 ?
 
 
 def ndlqr_ShouldCalcLambda(tree,index,i):
   node = tree.node_list + index
-  is_start = i == node.left_inds.start or i == node.right_inds.start
+  is_start = i == node.left_inds_start or i == node.right_inds_start
   return not is_start or i == 0
 
 def ndlqr_ComputeShurCompliment(solver,index,level,upper_level):
   node = solver.tree.node_list + index
-  left_start = node.left_inds.start
-  right_stop = node.right_inds.stop
+  left_start = node.left_inds_start
+  right_stop = node.right_inds_stop
   fact = solver.fact
   if upper_level == 0:
     soln = solver.soln
