d0ac47bbd08ab16c48cdec3830a3ecaf24d1e022
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-06-08 11:22:49 -0400

    Update diag_Matrix_set.cuh

--- SRI-23/help_functions/diag_Matrix_set.cuh
+++ SRI-23/help_functions/diag_Matrix_set.cuh
@@ -4,7 +4,7 @@ namespace cgrps = cooperative_groups;
 
 /* @brief convert vector v_Q to diagonal matrix n*n m_Q
 *  @params:
-*  unit32_t  n lenght of vector v_Q
+*  std::unit32_t  n lenght of vector v_Q
 *  T *v_Q vector of length n
 *  T *m_Q matrix n*n to change the diagonal
 */
@@ -12,7 +12,7 @@ namespace cgrps = cooperative_groups;
 
 template <typename T>
 __device__
-void diag_Matrix_set(T *v_Q, T *m_Q, unit32_t  n, cgrps::thread_group g) {
+void diag_Matrix_set(std::unit32_t  n, T *v_Q, T *m_Q,  cgrps::thread_group g) {
     
     for(uint32_t ind = g.thread_rank(); ind < n; ind+= g.size()){
         m_Q[ind*n+ind] = v_Q[n];        
