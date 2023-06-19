844b0fd3e9948b2937334cc5154bc9c588f0fc70
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-06-19 12:57:10 +0300

    Update set_const.cuh
    fixed the error of 
     cgrps::this_thread_block()

--- SRI-23/help_functions/set_const.cuh
+++ SRI-23/help_functions/set_const.cuh
@@ -7,7 +7,7 @@ __device__
 void set_const(std::uint32_t n, 
           T alpha, 
           T *x, 
-          cgrps::thread_group g)
+          cgrps::thread_group g =  cgrps::this_thread_block())
 {
     for(std::uint32_t ind = g.thread_rank(); ind < n; ind += g.size()){
         x[ind] = alpha;
