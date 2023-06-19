d11c406c15b4fc84a692946e779a532720213aac
Yana Botvinnik <98610778+yb2462@users.noreply.github.com>
2023-06-19 13:40:29 +0300

    Create chol_SolveInPlace.cuh
    Added missing function from google collab

---
+++ SRI-23/chol_SolveInPlace.cuh
@@ -0,0 +1,10 @@
+#include <cstdint>
+#include <cmath>
+
+//Solves linear system of equations with Cholesky
+template <typename T>
+__device__
+void cholSolve_InPlace(T *s_A, T *s_b, bool istransposed, int n, int m) {
+    lowerBackSub_InPlace<T>(s_A, s_b, 0, n, m);
+    lowerBackSub_InPlace<T>(s_A, s_b, 1, n, m);
+}
