5e86bfdb481e4d9d2895a1e947dc6573f8067177
EmreAdabag <48638847+EmreAdabag@users.noreply.github.com>
2023-06-27 09:43:02 -0400

    fix the dumb thing I did

--- SRI-23/help_functions/set_const.cuh
+++ SRI-23/help_functions/set_const.cuh
@@ -1,17 +1,15 @@
 #include <cstdint>
 #include <cooperative_groups.h>
-namespace cgrps = cooperative_groups;
 
 template <typename T>
 __device__
 void set_const(std::uint32_t n, 
           T alpha, 
-          T *x, 
-          cgrps::thread_group g =  cgrps::this_thread_block())
+          T *x)
 {
     printf("hi!\n");
    
-    for(std::uint32_t ind = g.thread_rank(); ind < n; ind += g.size()){
+    for(std::uint32_t ind = threadIdx.x; ind < n; ind += blockDim.x){
         printf("inside\n");
         x[ind] = alpha;
         printf(" #2\n");
