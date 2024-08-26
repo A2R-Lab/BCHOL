4b166ebe4dd2b07fe24630a0e496a0ca1f2bbea9
Yana Botvinnik <yb2462@barnard.edu>
2024-08-26 11:55:19 -0400

    python file to run pendulum+clean up for solve

--- python_vs/solve.py
+++ python_vs/solve.py
@@ -87,6 +87,7 @@ print("nstates",nstates)
 print("ninputs",ninputs)
 
 
+
 #transform the lists to numpy arrays
 Q =np.array([np.diag(row) for row in Q_list]) 
 R = np.array([np.diag(row) for row in R_list])
@@ -94,21 +95,9 @@ q = np.array(q_list)
 r = np.array(r_list)
 A = np.array(A_list)
 B = np.array(B_list)
-
-"""
-check if you need to transpose
-B = np.zeros((nhorizon,nstates,ninputs)) 
-
-#reshape B 
-for i in range(B.shape[0]):
-    first_part = B_v[i,:,:ninputs]
-    second_partt = B_v[i,:,ninputs:]
-    B[i] = np.vstack((first_part,second_partt))
-"""
 d = np.array(d_list)
 c = np.array(c_list)
 depth = int(math.log2(nhorizon))
-
 nhorizon=int(nhorizon)
 nstates= int(nstates)
 ninputs=int(ninputs)
@@ -123,11 +112,6 @@ if(INIT):
         print(f"q  {q[i]}")
         print(f"r {r[i]}")
         print(f"d {d[i]}")
-
-
-
-#A = A.reshape(-1,A.shape[-1]) #makes a long list of A matrices
-
 
 #imitating calling the kernel
 solve_kernel.solve_kernel(nhorizon,ninputs,nstates,Q,R,q,r,A,B,d)
